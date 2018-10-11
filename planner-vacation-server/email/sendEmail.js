d6a378e5a71ed1a2729bfb111a56acae803deac8
aleksandarratkov <aleksandar.ratkov@codecentric.de>
2018-10-11 17:39:51 +0800

    Add handler for sending emails

---
+++ planner-vacation-server/email/sendEmail.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const nodemailer = require("nodemailer");
+const ses = new AWS.SES();
+
+module.exports.sendEmail = (event, context, callback) => {
+	let data = JSON.parse(event.body);
+	let welcomeTemplate = `<p>Welcome <b>${data.username}</b> ${data.text}</p>`;
+	let vacationTemplate = `<p>You successfully created vacation request with next parameters:</p><ol><li>Dates: ${data.startDate} - ${data.endDate}</li><li>Number of days: ${data.numberOfDays}</li><li>Description: ${data.description}</li></ol>`;
+
+	let mailOptions = {
+		from: data.email,
+		subject: data.title,
+		to: data.email,
+		// bcc: Any BCC address you want here in an array,
+	};
+
+	if (data.type === 'welcome')
+	{
+		mailOptions.html = welcomeTemplate;
+	}
+	else if (data.type === 'vacation')
+	{
+		mailOptions.html = vacationTemplate;
+	}
+
+	// create Nodemailer SES transporter
+	let transporter = nodemailer.createTransport(
+		{
+			SES: ses
+		});
+
+	// send email
+	transporter.sendMail(mailOptions, function (err, info) {
+		if (err)
+		{
+			console.log("Error sending email");
+			callback(err);
+		}
+		else
+		{
+			console.log("Email sent successfully");
+			callback();
+		}
+	});
+};
